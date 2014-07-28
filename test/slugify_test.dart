6f7011cd4cf9b89046f273a6cff719effcfb789b
yosh <yoshtalwar@gmail.com>
2014-07-29 02:06:05 +0530

    slightly better tests

--- test/slugify_test.dart
+++ test/slugify_test.dart
@@ -1,13 +1,14 @@
 import 'package:slugify/slugify.dart';
 
 void main() {
-  var slugify = new Slugify();
-  List slugs = [
-    'Hello            World????????',
-    'aáäâeéëeiíiîoóöőôuúüűunç·/_,:;',
-  ];
 
-  for (var slug in slugs) {
-    print( slugify.slugify(value: slug) );
-  }
+  var case1 = new Slugify();
+  print( case1.slugify('Robert\'); DROP TABLE Students;--') );
+
+  var case2 = new Slugify(delimiter: '*');
+  print( case2.slugify('u wot m8? i\'ll hook u in   \\the\\ (gabber) m8 i sw/ear on `me` m.u.m.') );
+
+  var case3 = new Slugify(lowercase: false);
+  print( case3.slugify('NO I CAN\'T STOP YELLING CAUSE THAT\'S HOW I TALK!') );
+
 }
