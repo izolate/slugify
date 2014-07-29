c9b96ef52d70fb40e03d4a5e1b4c5e16edf971d3
yosh <yoshtalwar@gmail.com>
2014-07-29 15:23:28 +0530

    Created example

---
+++ example/slugify_example.dart
@@ -0,0 +1,17 @@
+import 'package:slugify/slugify.dart';
+
+void main() {
+
+  // slugify instances with different config
+  var slugify1 = new Slugify();
+  var slugify2 = new Slugify(lowercase: false);
+  var slugify3 = new Slugify(delimiter: '*');
+  var slugify4 = new Slugify(delimiter: ' ಠ_ಠ ', lowercase: false);
+
+  print( slugify1.slugify('This is a standard post title') );
+  print( slugify1.slugify('Robert\'); DROP TABLE Students;--') );
+  print( slugify2.slugify('NO I CAN\'T **STOP** {YELL}ING CAUSE THAT\'S HOW I TALK!') );
+  print( slugify3.slugify('u wot m8? i\'ll hook u in   \\the\\ (gabber) m8 i sw/er on `me` m.u.m.') );
+  print( slugify4.slugify('VOULEZ-VOUS COUCHER AVEC MOI, s\'il te plaît?') );
+
+}
