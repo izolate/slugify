82728b7fbfb609eb89ec0d1ff793ec756d87867c
yosh <yoshtalwar@gmail.com>
2019-04-24 14:34:11 +0700

    Fix comments in example

--- example/slugify_example.dart
+++ example/slugify_example.dart
@@ -1,19 +1,19 @@
 import 'package:slugify/slugify.dart';
 
 void main() {
-  // Slugify with default config
+  // Slugify with default config.
   var slug1 = Slugify('Hello, world!');
   print(slug1);
 
-  // Set a custom delimiter
+  // Set a custom delimiter.
   var slug2 = Slugify('Some prefer snake case', delimiter: '_');
   print(slug2);
 
-  // Preserve the original case
+  // Preserve the original case.
   var slug3 = Slugify('BREAKING NEWS', lowercase: false);
   print(slug3);
 
-  // Normalize to latin characters
+  // Normalize to latin characters.
   var slug4 = Slugify("C'est déjà l'été.");
   print(slug4);
 }
