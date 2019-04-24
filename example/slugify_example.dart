652f9ed6be48359600f12b2c2276aaf6b6a1821c
yosh <yoshtalwar@gmail.com>
2019-04-24 14:36:58 +0700

    Add print examples

--- example/slugify_example.dart
+++ example/slugify_example.dart
@@ -3,17 +3,17 @@ import 'package:slugify/slugify.dart';
 void main() {
   // Slugify with default config.
   var slug1 = Slugify('Hello, world!');
-  print(slug1);
+  print(slug1); // 'hello-world'
 
   // Set a custom delimiter.
   var slug2 = Slugify('Some prefer snake case', delimiter: '_');
-  print(slug2);
+  print(slug2); // 'some_prefer_snake_case'
 
   // Preserve the original case.
-  var slug3 = Slugify('BREAKING NEWS', lowercase: false);
-  print(slug3);
+  var slug3 = Slugify('BREAKING NEWS! The world is ending!', lowercase: false);
+  print(slug3); // 'BREAKING-NEWS-The-world-is-ending'
 
   // Normalize to latin characters.
   var slug4 = Slugify("C'est déjà l'été.");
-  print(slug4);
+  print(slug4); // 'cest-deja-lete'
 }
